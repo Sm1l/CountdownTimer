60a50e8257fa9ea962d0d9ab30d677082fc400f3
Sm1le <Onekonstantin@gmail.com>
2023-10-09 20:20:55 +0300

    new count func

--- src/components/CountContainer/CountContainer.tsx
+++ src/components/CountContainer/CountContainer.tsx
@@ -3,17 +3,23 @@ import React, { useEffect, useState } from "react";
 import styles from "./CountContainer.module.scss";
 import { CountItem } from "../CountItem";
 
-interface CountContainerProps {}
+interface CountContainerProps {
+  finalDate: number;
+  setCountIsVisible: React.Dispatch<React.SetStateAction<boolean>>;
+}
 
-const CountContainer: React.FC<CountContainerProps> = () => {
-  const finalDate = new Date("2024 Jan 1 00:00:00");
-  // const finalDate = new Date("2023 Oct 8 17:14:00");
-  const diff: boolean = +finalDate > Date.now();
+const CountContainer: React.FC<CountContainerProps> = ({ finalDate, setCountIsVisible }) => {
   const [timeLeft, setTimeLeft] = useState<number>(0);
 
   useEffect(() => {
-    if (diff) {
-      setTimeLeft(+finalDate - Date.now());
+    if (finalDate > Date.now()) {
+      setTimeLeft(finalDate - Date.now());
+      const interval = setInterval(() => {
+        setTimeLeft((timeLeft) => (timeLeft >= 1000 ? finalDate - Date.now() : 0));
+        setCountIsVisible(finalDate > Date.now());
+        console.log("setInterval");
+      }, 1000);
+      return () => clearInterval(interval);
     }
   }, []);
 
@@ -29,30 +35,13 @@ const CountContainer: React.FC<CountContainerProps> = () => {
     setSeconds(Math.floor(timeLeft / 1000) % 60);
   }, [timeLeft]);
 
-  useEffect(() => {
-    if (timeLeft >= 0) {
-      const interval = setInterval(() => {
-        setTimeLeft((timeLeft) => (timeLeft >= 1000 ? timeLeft - 1000 : 0));
-      }, 1000);
-      return () => {
-        clearInterval(interval);
-      };
-    }
-  }, []);
-
   return (
-    <>
-      {diff ? (
-        <div className={styles.countContainer}>
-          <CountItem number={days} text="days" />
-          <CountItem number={hours} text="hours" />
-          <CountItem number={minutes} text="minutes" />
-          <CountItem number={seconds} text="seconds" />
-        </div>
-      ) : (
-        <h2 className={styles.title}>It's time!</h2>
-      )}
-    </>
+    <div className={styles.countContainer}>
+      <CountItem number={days} text="days" />
+      <CountItem number={hours} text="hours" />
+      <CountItem number={minutes} text="minutes" />
+      <CountItem number={seconds} text="seconds" />
+    </div>
   );
 };
 
--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 import styles from "./Main.module.scss";
 import { CountContainer } from "../CountContainer";
@@ -6,10 +6,18 @@ import { CountContainer } from "../CountContainer";
 interface MainProps {}
 
 const Main: React.FC<MainProps> = () => {
+  const finalDate: number = +new Date("2024 Jan 1 00:00:00");
+  // const finalDate = +new Date("2023 Oct 9 20:08:40");
+  const [countIsVisible, setCountIsVisible] = useState(true);
+
   return (
     <main className={styles.main}>
-      <h1 className={styles.title}>we`re launching soon</h1>
-      <CountContainer />
+      {countIsVisible ? (
+        <h1 className={styles.title}>we`re launching soon</h1>
+      ) : (
+        <h1 className={styles.title}>happy new year!</h1>
+      )}
+      {countIsVisible && <CountContainer finalDate={finalDate} setCountIsVisible={setCountIsVisible} />}
     </main>
   );
 };
